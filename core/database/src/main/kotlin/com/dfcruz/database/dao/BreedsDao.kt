100af3a95d3a4daa562030f668f8a394cbe6a207
dfcruz <diogo98cruzdev@gmail.com>
2024-01-16 18:36:43 +0000

    Add generic queries to BreedsDao

--- core/database/src/main/kotlin/com/dfcruz/database/dao/BreedsDao.kt
+++ core/database/src/main/kotlin/com/dfcruz/database/dao/BreedsDao.kt
@@ -1,8 +1,36 @@
 package com.dfcruz.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.dfcruz.database.entity.BreedEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BreedsDao {
+
+    @Query("SELECT * FROM breed WHERE id = :id")
+    suspend fun get(id: String): BreedEntity
+
+    @Query("SELECT * FROM breed")
+    fun getAll(): Flow<List<BreedEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(breed: BreedEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(breeds: List<BreedEntity>)
+
+    @Update
+    suspend fun update(breed: BreedEntity)
+
+    @Delete
+    suspend fun delete(breed: BreedEntity)
+
+    @Query("DELETE FROM breed")
+    suspend fun clearAll()
 
 }
