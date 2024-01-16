56665a35b70f53da0b658468c2225d12733bb6e7
dfcruz <diogo98cruzdev@gmail.com>
2024-01-16 18:35:28 +0000

    Create Breed entity

--- core/database/src/main/kotlin/com/dfcruz/database/entity/BreedEntity.kt
+++ core/database/src/main/kotlin/com/dfcruz/database/entity/BreedEntity.kt
@@ -1,10 +1,22 @@
 package com.dfcruz.database.entity
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "breed")
 data class BreedEntity(
     @PrimaryKey(autoGenerate = false)
-    val id: String
+    val id: String,
+    val name: String,
+    val temperament: String,
+    val origin: String,
+    val countryCodes: String,
+    val countryCode: String,
+    val lifeSpan: String,
+    val wikipediaUrl: String,
+    @Embedded
+    val imageEntity: ImageEntity,
+    @Embedded
+    val weight: MassUnitEntity
 )
